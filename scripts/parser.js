631deb2dcbcedfb781aa705bd233a7fcd87a6adc
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-07-06 17:14:31 +0400

    changed parser to promisified ajax call

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,6 +1,21 @@
 const parser = {
-    dataFetch: async (json) => {
-        let response = await fetch(json);
-        return await response.json();
-    }
+	dataFetch: async (json) =>
+	{
+		return new Promise((resolve, reject) =>
+		{
+			$.ajax({
+				type: 'GET',
+				dataType: 'json',
+				url: json,
+				success: function (data)
+				{
+					resolve(data);
+				},
+				error: function (error)
+				{
+					reject(error);
+				},
+			});
+		})
+	}
 }
