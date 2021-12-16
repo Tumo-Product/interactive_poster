e12726b94ff9e67b23420a3c4f002898a9a53284
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-12-16 11:41:12 +0400

    server changed

--- scripts/config.js
+++ scripts/config.js
@@ -1,7 +1,7 @@
 let href = window.location.href;
 href     = href.substring(0, href.indexOf("?"));
 
-axios.defaults.baseURL  = "https://blackboxbasic.herokuapp.com/";
+axios.defaults.baseURL  = "https://content-tools.tumo.world:4000/";
 
 let config = {
     query_url           : "interactive_poster/getsetsone?_uid="
